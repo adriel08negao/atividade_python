06214e620d0ecc3c4d19b7e470f77cf960d9d557
Adriel dos Santos da Silva <adrieldossantossilva10@gmail.com>
2026-03-30 16:13:06 -0300

    atividade 2 fibonacci

---
+++ aula06/atv.fibonacci.py
@@ -0,0 +1,19 @@
+
+contagem = int(input("Digite até que número contar: "))
+
+anterior = 0
+atual = 1
+proximo = 0
+
+print("Sequência fibonacci")
+
+for i in range(contagem+1):
+    print(anterior)
+    proximo = anterior+atual
+    anterior = atual
+    atual = proximo
+
+
+
+
+
