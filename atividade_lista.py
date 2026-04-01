2d4ea0928c27b0bbc242a989a9852adb424d32bb
Adriel dos Santos da Silva <adrieldossantossilva10@gmail.com>
2026-04-01 13:33:58 -0300

    Atividade lista

--- atividade_lista.py
+++ atividade_lista.py
@@ -3,20 +3,20 @@ idade = []
 altura = []
 alunos = 0
 
-for i in range(30):
-    idade = int(input("Digite sua idade: "))
-    altura = float(input("Digite sua altura em metros: "))
+for i in range(3):
+    numero = int(input("Digite sua idade: "))
+    tamanho = float(input("Digite sua altura em metros: "))
 
-    idade.append(idade)
-    altura.append(altura)
+    idade.append(numero)
+    altura.append(tamanho)
 
-media = sum(altura) / 30
+media = sum(altura) / 3
 
-for i in range(30):
+for i in range(3):
     
     if idade[i]>13 and altura[i]<media:
        alunos+=1
-print(f"A quantidade de alunos {alunos}")
+print(f"A quantidade de alunos {alunos}, a media {media:.2f}")
 
      
 
